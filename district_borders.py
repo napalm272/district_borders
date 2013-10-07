c9877c089cca85e1eb50eeddc2f794ab239add70
Christian Oudard <christian.oudard@gmail.com>
2013-10-07 16:55:45 -0700

    Add implementation of algorithm.

---
+++ district_borders.py
@@ -0,0 +1,95 @@
+def polygons_to_graph(polygons):
+    graph = {}
+
+    # Initialize graph nodes.
+    for poly in polygons:
+        for p in poly:
+            graph[p] = set()
+
+    # Add edges.
+    for poly in polygons:
+        size = len(poly)
+        for i in range(size + 1):
+            a = poly[i % size]
+            b = poly[(i + 1) % size]
+            graph[a].add(b)
+            graph[b].add(a)
+
+    return graph
+
+
+def merge_corners_onto_borders(graph):
+    """
+    """
+    # Iterate over the border line segments, and find which grid points each
+    # one passes through.
+    points = set(graph.keys())
+    edges = graph_edges(graph)
+    while edges:
+        a, b = edges.pop()
+        for p in segment_lattice_points(a, b):
+            if p in points:
+                # We have found a corner that is exactly on a border. Split the
+                # border line and add in the new corner.
+                graph[a].remove(b)
+                graph[b].remove(a)
+                edges.add(tuple(sorted((a, p))))
+                graph[a].add(p)
+                graph[p].add(a)
+                edges.add(tuple(sorted((b, p))))
+                graph[b].add(p)
+                graph[p].add(b)
+                break
+
+
+def graph_edges(graph):
+    """
+    """
+    edges = set()
+    for node, neighbors in graph.items():
+        for neighbor in neighbors:
+            # Canonicalize the edge.
+            edges.add(tuple(sorted((node, neighbor))))
+    return edges
+
+
+def segment_lattice_points(a, b):
+    """
+
+    """
+    # Find the slope of the line segment as a lowest-terms fraction.
+    ax, ay = a
+    bx, by = b
+    dx = bx - ax
+    dy = by - ay
+
+    divisor = gcd(dx, dy)
+    step_x = dx // divisor
+    step_y = dy // divisor
+
+    # Iterate over points on the segment, excluding the start and end points.
+    points = []
+    x = ax + step_x
+    y = ay + step_y
+    while x != bx or y != by:
+        points.append((x, y))
+        x += step_x
+        y += step_y
+
+    return points
+
+
+def gcd(a, b):
+    """
+    Euclid's algorithm to find the greatest common denominator.
+
+    >>> gcd(12, 21)
+    3
+    >>> gcd(53473753604, 32407556409)
+    271
+    """
+    while b != 0:
+        temp = a % b
+        a = b
+        b = temp
+    return a
---
+++ test.py
@@ -0,0 +1,155 @@
+from nose.tools import assert_equal
+
+from district_borders import (
+    polygons_to_graph,
+    graph_edges,
+    segment_lattice_points,
+    merge_corners_onto_borders,
+)
+
+
+def test_polygons_to_graph():
+    #   012345
+    # 0 A--B-C
+    # 1 |  | |
+    # 2 D--E-F
+    # 3 |    |
+    # 4 |    |
+    # 5 G----H
+    a = (0, 0)
+    b = (3, 0)
+    c = (5, 0)
+    d = (0, 2)
+    e = (3, 2)
+    f = (5, 2)
+    g = (0, 5)
+    h = (5, 5)
+    polygons = [
+        [a, b, e, d],
+        [b, c, f, e],
+        [d, f, h, g],
+    ]
+    graph = polygons_to_graph(polygons)
+    assert_equal(
+        graph,
+        {
+            a: {b, d},
+            b: {a, c, e},
+            c: {b, f},
+            d: {a, e, f, g},
+            e: {b, d, f},
+            f: {c, e, d, h},
+            g: {d, h},
+            h: {g, f},
+        }
+    )
+
+
+def test_graph_edges():
+    a, b, c, d, e = range(5)
+
+    cases = [
+        (
+            {a: {b}, b: {a}},
+            [(a, b)]
+        ),
+        (
+            {
+                a: {b}, b: {a},
+                c: {d}, d: {c},
+            },
+            [(a, b), (c, d)],
+        ),
+        (
+            {
+                a: {b, c},
+                b: {a, c},
+                c: {a, b},
+            },
+            [(a, b), (a, c), (b, c)],
+        ),
+    ]
+    for graph, target_edges in cases:
+        def test(target_edges):
+            assert_equal(sorted(graph_edges(graph)), target_edges)
+        yield test, target_edges
+
+
+def test_segment_lattice_points():
+    assert_equal(
+        segment_lattice_points((0, 0), (0, 4)),
+        [(0, 1), (0, 2), (0, 3)],
+    )
+    assert_equal(
+        segment_lattice_points((2, 2), (5, 2)),
+        [(3, 2), (4, 2)],
+    )
+    assert_equal(
+        segment_lattice_points((-3, -3), (3, 3)),
+        [(-2, -2), (-1, -1), (0, 0), (1, 1), (2, 2)]
+    )
+    assert_equal(
+        segment_lattice_points((0, 0), (6, 12)),
+        [(1, 2), (2, 4), (3, 6), (4, 8), (5, 10)],
+    )
+    assert_equal(
+        segment_lattice_points((0, 0), (4, 6)),
+        [(2, 3)],
+    )
+    assert_equal(
+        segment_lattice_points((0, 0), (5, 13)),
+        [],
+    )
+
+
+def test_merge_corners_onto_borders():
+    #   01234567
+    # 0 A-B-C
+    # 1   |
+    # 2   D
+    a = (0, 0)
+    b = (2, 0)
+    c = (4, 0)
+    d = (2, 2)
+    graph = {
+        a: {c}, c: {a},
+        b: {d}, d: {b},
+    }
+    merge_corners_onto_borders(graph)
+    assert_equal(
+        graph,
+        {
+            a: {b},
+            b: {a, c, d},
+            c: {b},
+            d: {b},
+        }
+    )
+
+    #   01234567
+    # 0 A-B-C-D
+    # 1   | |
+    # 2   E F
+    a = (0, 0)
+    b = (2, 0)
+    c = (4, 0)
+    d = (6, 0)
+    e = (2, 2)
+    f = (4, 2)
+    graph = {
+        a: {d}, d: {a},
+        b: {e}, e: {b},
+        c: {f}, f: {c},
+    }
+    merge_corners_onto_borders(graph)
+    assert_equal(
+        graph,
+        {
+            a: {b},
+            b: {a, c, e},
+            c: {b, d, f},
+            d: {c},
+            e: {b},
+            f: {c},
+        }
+    )
